phoebe-project/phoebe-project.org

commit 6445fd4a1e7827844e2dc502f6c67ec88e310ac4

link to py and ipynb in github pages instead of raw.githubusercontent
this way they can be downloadable without requiring javascript (which was resulting in href links to long and truncated files) or without being rendered in plain text in the browser by raw.githubusercontent.

src/githubcontent.jsx
@@ -34,6 +34,10 @@ export class GitHubContent extends Component {
       var contentURL = "https://github.com/phoebe-project/"+repo+"/blob/"+branch+"/"+path
       var contentURLRawDir = "https://raw.githubusercontent.com/phoebe-project/"+repo+"/"+branch+"/"+path.split('/').slice(0,-1)
       var contentURLRaw = "https://raw.githubusercontent.com/phoebe-project/"+repo+"/"+branch+"/"+path
+      if (repo === 'phoebe2-docs' && (path.endsWith(".py") || path.endsWith(".ipynb"))) {
+        var contentURLRawDir = "https://phoebe-project.github.io/"+repo+"/"+branch+"/"+path.split('/').slice(0,-1)
+        var contentURLRaw = "https://phoebe-project.github.io/"+repo+"/"+branch+"/"+path
+      }
 
       var contentType = null;
       var extension = path.split('.').slice(-1)[0];
@@ -98,10 +102,10 @@ export class GitHubContent extends Component {
 
         // ok... this is a bit hardcoded to our specific situation.  Probably should handle this with a callback of some sort
         dl_html = <div>
-                    <Link to={this.state.contentURLRaw} downloadFilename={slug+".ipynb"}>Download IPython Notebook: {slug}.ipynb</Link>
+                    <Link to={this.state.contentURLRaw}>Download IPython Notebook: {slug}.ipynb</Link>
                     &nbsp;(<Link to={"/help/ipynb"}>ipynb help</Link>)
                     <br/>
-                    <Link to={this.state.contentURLRaw.split(slug+".ipynb")[0]+"/py/"+slug+".py"} downloadFilename={slug+".py"}>Download Python Script: {slug}.py</Link>
+                    <Link to={this.state.contentURLRaw.split(".ipynb")[0]+".py"}>Download Python Script: {slug}.py</Link>
                     <br/>
                     <Link to={"https://colab.research.google.com/github/phoebe-project/"+this.props.repo+"/blob/"+this.props.branch+"/"+this.props.path} hideExternal={true}><span className="far fa-fw fa-play-circle"></span> Open in Colab live-session</Link>
                     &nbsp;(<Link to={"/quickstart/"+this.props.branch}>colab help</Link>)

src/common.jsx
@@ -93,23 +93,10 @@ export class Link extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      downloadContent: null,
       href: null,
     };
   }
   componentDidMount() {
-    if (this.props.downloadFilename) {
-      fetch(processLink(this.props.to))
-        .catch(() => this.setState({content: null}))
-        .then(res => {
-          if (res.ok) {
-            return res.text();
-          } else {
-            return null;
-          }
-        })
-        .then(content => this.setState({downloadContent: content}))
-      }
   }
   render() {
     var to = processLink(this.props.to)
@@ -118,21 +105,13 @@ export class Link extends Component {
     }
 
     if (to.startsWith("http") || to.startsWith("ftp")) {
-      if (this.props.downloadFilename) {
-        if (this.state.downloadContent) {
-          return (
-            <a {...this.props} href={"data:text/plain,"+this.state.downloadContent} type="text/x-python" download={this.props.downloadFilename}><span className="fas fa-fw fa-file-download"></span> {this.props.children}</a>
-          )
-        } else {
-          return (
-            <p style={{display: "inline"}}>{this.props.children}</p>
-          )
-        }
-      } else {
-        return (
-          <a {...this.props} href={to} target="blank" target="_blank" rel="noopener noreferrer">{this.props.hideExternal ? null : <span className="fas fa-fw fa-external-link-alt"> </span>}{this.props.children}</a>
-        )
+      var icon = 'fa-external-link-alt';
+      if (to.endsWith("py") || to.endsWith("ipynb")) {
+        icon = 'fa-file-download';
       }
+      return (
+        <a {...this.props} href={to} target="blank" target="_blank" rel="noopener noreferrer">{this.props.hideExternal ? null : <span className={"fas fa-fw "+icon}> </span>}{this.props.children}</a>
+      )
     } else if (to.startsWith(process.env.PUBLIC_URL) || to.startsWith("/static/")) {
       return (
         <a {...this.props} href={to}>{this.props.children}</a>

src/contribute.jsx
@@ -325,7 +325,7 @@ export class ContributeDevelopment extends Component {
           </p>
 
           <ul>
-            <li>master: the master branch is intentionally left blank with a README explaining the branch structure.</li>
+            <li>master: the master branch is intentionally left blank (except for built .py and .ipynb files hosted by GitHub pages) with a README explaining the branch structure.</li>
             <li>2.0, 2.1, etc: a branch exists for each version of PHOEBE 2.  It is important when making changes to tutorials, etc, to commit and push to all applicable branches.  This way we can keep the documentation for older releases current.</li>
             <li>development, etc: branches can exist in the phoebe2-docs repository that correspond to branches (particularly feature branches) in the main phoebe2 repository.  These exist to prepare documentation for future releases (without knowing the release number yet).</li>
           </ul>

commit cc784e90712a0663d3dbe6981d5fe9f347cda8f9

fix showing static png and gifs
bug introduced in 6445fd4a1e7827844e2dc502f6c67ec88e310ac4.  Images are not ever hosted by github pages, even though the .py and .ipynb files are

src/githubcontent.jsx
@@ -33,6 +33,7 @@ export class GitHubContent extends Component {
     if (repo && path) {
       var contentURL = "https://github.com/phoebe-project/"+repo+"/blob/"+branch+"/"+path
       var contentURLRawDir = "https://raw.githubusercontent.com/phoebe-project/"+repo+"/"+branch+"/"+path.split('/').slice(0,-1)
+      var contentURLRawDirImages = "https://raw.githubusercontent.com/phoebe-project/"+repo+"/"+branch+"/"+path.split('/').slice(0,-1)
       var contentURLRaw = "https://raw.githubusercontent.com/phoebe-project/"+repo+"/"+branch+"/"+path
       if (repo === 'phoebe2-docs' && (path.endsWith(".py") || path.endsWith(".ipynb"))) {
         var contentURLRawDir = "https://phoebe-project.github.io/"+repo+"/"+branch+"/"+path.split('/').slice(0,-1)
@@ -66,7 +67,7 @@ export class GitHubContent extends Component {
             for (var i=0; i<content.cells.length; i++) {
               if (content.cells[i]['cell_type'] === 'markdown') {
                 for (var j=0; j<content.cells[i].source.length; j++) {
-                  content.cells[i].source[j] = content.cells[i].source[j].replace(/([a-z,_,0-9,-,\+]*).(gif|png)/, `${contentURLRawDir}/$1.$2`)
+                  content.cells[i].source[j] = content.cells[i].source[j].replace(/([a-z,_,0-9,-,\+]*).(gif|png)/, `${contentURLRawDirImages}/$1.$2`)
                 }
               }
             }