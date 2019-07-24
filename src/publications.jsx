b76a9330141199d69ff4060b92044a55e3901f9c
Kyle Conroy <kyleconroy@gmail.com>
2019-07-24 09:44:43 -0400

    update URL to published version of Kabath et al and Abdul Masih et al

--- src/publications.jsx
+++ src/publications.jsx
@@ -173,8 +173,8 @@ export class Publications extends Component {
 
           {/* Look through citations in release papers since those listed below, confirm that citation is by "using" PHOEBE 2 instead of just mentioning it, and add new entries to the top */}
 
-          <Publication author="Abdul-Masih et al. (2019)" adsLink="https://ui.adsabs.harvard.edu/abs/2019arXiv190601066A" title="Clues on the Origin and Evolution of Massive Contact Binaries: Atmosphere Analysis of VFTS 352"/>
-          <Publication author="Kabath et al. (2019)" adsLink="https://ui.adsabs.harvard.edu/abs/2019arXiv190504665K" title="Detection limits of exoplanetary atmospheres with 2-m class telescopes"/>
+          <Publication author="Abdul-Masih et al. (2019)" adsLink="https://ui.adsabs.harvard.edu/abs/2019ApJ...880..115A" title="Clues on the Origin and Evolution of Massive Contact Binaries: Atmosphere Analysis of VFTS 352"/>
+          <Publication author="Kabath et al. (2019)" adsLink="https://ui.adsabs.harvard.edu/abs/2019PASP..131h5001K" title="Detection limits of exoplanetary atmospheres with 2-m class telescopes"/>
           <Publication author="Jones et al. (2019)" adsLink="https://ui.adsabs.harvard.edu/?#abs/2019MNRAS.482L..75J" title="The short orbital period binary star at the heart of the planetary nebula M 3-1★"/>
           <Publication author="Boffin et al. (2018)" adsLink="https://ui.adsabs.harvard.edu/?#abs/2018A&A...619A..84B" title="When nature tries to trick us. An eclipsing eccentric close binary superposed on the central star of the planetary nebula M 3-2"/>
           <Publication author="Papageorgiou et al. (2018)" adsLink="https://ui.adsabs.harvard.edu/?#abs/2018ApJS..238....4P" title="An Updated Catalog of 4680 Northern Eclipsing Binaries with Algol-type Light-curve Morphology in the Catalina Sky Surveys"/>
