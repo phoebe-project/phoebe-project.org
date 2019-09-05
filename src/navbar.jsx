9d0f69d49173977d972d7aadd2a8fd0a6316a75f
Kyle Conroy <kyleconroy@gmail.com>
2019-09-05 15:01:22 -0400

    consolidate source and contribute into "develop" dropdown menu item
    to prepare for future additions of "client" and "tables" to the navbar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -61,19 +61,30 @@ export class Navbar extends Component {
               <li><NavLink to="/releases" style={navbarLinkStyle} title="Releases"><span className="fa fa-fw fa-tags"></span> <span className="hidden-sm">Releases</span></NavLink></li>
               <li><NavLink to="/install" style={navbarLinkStyle} title="Download & Install"><span className="fa fa-fw fa-download"></span> <span className="hidden-sm">Install</span></NavLink></li>
               {/* <li><NavLink to="/clients" style={navbarLinkStyle} title="Interactive Clients"><span className="fa fa-fs fa-desktop"></span> <span className="hidden-sm">Clients</span></NavLink></li> */}
+              {/* <li><NavLink to="/tables" style={navbarLinkStyle} title="Passband & Atmosphere Tables"><span className="fa fa-fs fa-th"></span> <span className="hidden-sm">Tables</span></NavLink></li> */}
               <li><NavLink to="/docs" style={navbarLinkStyle} title="Documentation"><span className="fa fa-fw fa-book-open"></span> <span className="hidden-sm hidden-xs">Docs</span><span className="visible-xs-inline">Documentation</span></NavLink></li>
-              <li><NavLink to="/news" style={navbarLinkStyle} title="News"><span className="far fa-fw fa-newspaper"></span> <span className="hidden-sm">News</span></NavLink></li>
-              <li><NavLink to="/publications" style={navbarLinkStyle} title="Publications"><span className="fas fa-fw fa-copy"></span> <span className="hidden-sm">Publications</span></NavLink></li>
-              <li><NavLink to="/workshops" style={navbarLinkStyle} title="Workshops"><span className="fas fa-fw fa-chalkboard-teacher"></span> <span className="hidden-md hidden-sm">Workshops</span></NavLink></li>
-              <li><NavLink to="/source" style={navbarLinkStyle} title="Source-Code"><span className="fas fa-fw fa-code-branch"></span> <span className="hidden-md hidden-sm">Source</span></NavLink></li>
-              <li><NavLink to="/contribute" style={navbarLinkStyle} title="Contribute"><span className="fas fa-fw fa-user-plus"></span> <span className="hidden-md hidden-sm">Contribute</span></NavLink></li>
+              <li><NavLink to="/news" style={navbarLinkStyle} title="News"><span className="far fa-fw fa-newspaper"></span> <span className="hidden-sm hidden-md">News</span></NavLink></li>
+              <li><NavLink to="/publications" style={navbarLinkStyle} title="Publications"><span className="fas fa-fw fa-copy"></span> <span className="hidden-sm hidden-md">Publications</span></NavLink></li>
+              <li><NavLink to="/workshops" style={navbarLinkStyle} title="Workshops"><span className="fas fa-fw fa-chalkboard-teacher"></span> <span className="hidden-sm hidden-md">Workshops</span></NavLink></li>
+              <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown" title="Develop" style={navbarLinkStyle}><span className="fa fa-fw fa-code-branch"></span> <span className="hidden-sm hidden-md">Develop</span> <b className="caret"></b></a>
+                <ul className="dropdown-menu">
+                  <li><NavLink to="/source" style={navbarLinkStyle} title="Source-Code"><span className="fas fa-fw fa-code"></span> <span>Source Code</span></NavLink></li>
+                  <li><NavLink to="/contribute" style={navbarLinkStyle} title="Contribute"><span className="fas fa-fw fa-user-plus"></span> <span>Contribute</span></NavLink></li>
+                  <li><NavLink to="/development-team" style={navbarLinkStyle} title="PHOEBE Development Team"><span className="fa fa-fw fa-users"></span> Development Team</NavLink></li>
+                  <li><NavLink to="/contribute/development-guide" style={navbarLinkStyle} title="Development Guide"><span className="fas fa-fw fa-microscope"></span> Development Guide</NavLink></li>
+                  <li><NavLink to="/contribute#features" style={navbarLinkStyle} hideExternal={true} title="Request New Feature"><span className="fas fa-fw fa-flask"></span> Request New Feature</NavLink></li>
+                  <li><NavLink to="/contribute#issues" style={navbarLinkStyle} hideExternal={true} title="Report an Issue"><span className="fas fa-fw fa-bug"></span> Report an Issue</NavLink></li>
+                </ul>
+              </li>
+
               <li className="dropdown">
                 <a href="#" className="dropdown-toggle" data-toggle="dropdown" title="Help" style={navbarLinkStyle}><span className="fa fa-fw fa-question"></span> <span className="hidden-sm hidden-md">Help</span> <b className="caret"></b></a>
                 <ul className="dropdown-menu">
-                  <li><NavLink to="/development-team" style={navbarLinkStyle} title="PHOEBE Development Team"><span className="fa fa-fw fa-users"></span> Development Team</NavLink></li>
-                  <li><NavLink to="/help/contact" style={navbarLinkStyle} title="Mailing Lists & Contact Info"><span className="far fa-fw fa-envelope"></span> Mailing Lists</NavLink></li>
                   <li><NavLink to="/help/faq" style={navbarLinkStyle} title="Frequently Asked Questions"><span className="fa fa-fw fa-question"></span> FAQ</NavLink></li>
-                  <li><NavLink to="/contribute/development-guide" style={navbarLinkStyle} title="Development Guide"><span className="fas fa-fw fa-microscope"></span> Development Guide</NavLink></li>
+                  <li><NavLink to="/help/contact" style={navbarLinkStyle} title="Mailing Lists & Contact Info"><span className="far fa-fw fa-envelope"></span> Mailing Lists</NavLink></li>
+                  <li><NavLink to="/development-team" style={navbarLinkStyle} title="PHOEBE Development Team"><span className="fa fa-fw fa-users"></span> Development Team</NavLink></li>
+                  <li><NavLink to="/contribute#features" style={navbarLinkStyle} hideExternal={true} title="Request New Feature"><span className="fas fa-fw fa-flask"></span> Request New Feature</NavLink></li>
                   <li><NavLink to="/contribute#issues" style={navbarLinkStyle} hideExternal={true} title="Report an Issue"><span className="fas fa-fw fa-bug"></span> Report an Issue</NavLink></li>
                 </ul>
               </li>
